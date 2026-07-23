26b73a67526eeba91fcf5d41ef4c90977799cb76
NyxCyRy <NyxCyRy@users.noreply.github.com>
2026-07-22 20:02:52 -0400

    Update redirect URI for slopvibe-org fork

---
+++ build/gabbro/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 3 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Playback for Spotify",
+  .company = "alex_pavlov",
+  .icon_resource_id = RESOURCE_ID_IMAGE_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_PLATFORM_GABBRO,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x1C, 0x75, 0x7A, 0x48, 0xF5, 0xC3, 0x4F, 0x59, 0xBD, 0x13, 0x56, 0x18, 0xD6, 0x75, 0x0F, 0x73 },
+  .virtual_size = 0xb6b6
+};
